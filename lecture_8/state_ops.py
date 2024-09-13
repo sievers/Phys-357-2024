60b67a4a5764ed40ed7777e020c5106c63e2902a
JLS <jonathan.sievers@gmail.com>
2024-09-13 14:38:09 -0400

    updated code with in-class demos for rotation matrices

--- lecture_8/state_ops.py
+++ lecture_8/state_ops.py
@@ -11,6 +11,7 @@ xm=np.asarray([1,-1])/np.sqrt(2)
 yp=np.asarray([1,1J])/np.sqrt(2)
 ym=np.asarray([1,-1J])/np.sqrt(2)
 
+
 #let's build up the matrices of bras and kets, which
 #will come in handy 
 zket=np.zeros([2,2],dtype='complex')
@@ -21,9 +22,51 @@ xket=np.zeros([2,2],dtype='complex')
 xket[:,0]=xp
 xket[:,1]=xm
 
-zket=np.zeros([2,2],dtype='complex')
-zket[:,0]=yp
-zket[:,1]=ym
+yket=np.zeros([2,2],dtype='complex')
+yket[:,0]=yp
+yket[:,1]=ym
+
+
+#let's work out z->x basis
+#+x projection operator
+xplus_proj=np.outer(np.asarray([1,0]),np.conj(xp.T))
+xminus_proj=np.outer(np.asarray([0,1]),np.conj(xm.T))
+z2x=xplus_proj+xminus_proj
+
+
+print('plus x in x is ',z2x@xp)
+print('minus x in x is ',z2x@xm)
+
+print('plus z in x is ',z2x@zp)
+y_in_x=z2x@yp
+
+#if I write a complex numer as a+ib
+#I can also write it as c*exp(i theta)
+#np.angle(a+ib) returns theta
+theta=np.angle(y_in_x[0])
+y_in_x=y_in_x*np.exp(-1J*theta)
+print('plus y in x is ',y_in_x)
+
+#we want to take <y| y>
+print('normalization: ',np.dot(np.conj(y_in_x),y_in_x))
+
+
+print("\n Doing rotation about z now")
+M1=np.zeros([2,2],dtype='complex')
+M1[:,0]=xp
+M1[:,1]=yp
+
+M2=np.zeros([2,2],dtype='complex')
+M2[:,0]=yp
+M2[:,1]=xm
+
+R=M2@np.linalg.inv(M1)
+print('rotation matrix is: ',R)
+
+print('R@ym is ',R@ym)
+print('R@xm is ',R@xm)
+assert(1==0)
+                     
 
 #what should the associated bra matrices look like?
 
